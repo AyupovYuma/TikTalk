dea6b2d44f775472c6e4ec9e6d9f3210fb906247
AyupovYuma <148763068+AyupovYuma@users.noreply.github.com>
2025-01-27 20:24:58 +0300

    регистрация пользователя, проверенная

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,22 +10,27 @@ router = APIRouter()
 @router.post("/register")
 def registr(data: dict, db: Session = Depends(get_db)):
     try:
-        loggin = data.get("loggin")
+        loggin = data.get("loggin").lower()
         password = data.get("password")
 
         if not loggin or not password:
             raise HTTPException(status_code=400, detail="Missing loggin or password")
 
-        user_exists = db.query(User).filter(User.loggin == loggin).first()
+        user_exists = db.query(User).filter(User.loggin == loggin.lower()).first()
         if user_exists:
             raise HTTPException(status_code=400, detail="login_is_busy")
 
         new_user = User(loggin=loggin, password_hash=hash_password(password))
         db.add(new_user)
-        db.commit()
+        try:
+            db.commit()
+        except:
+            db.rollback()
+            raise HTTPException(status_code=400, detail='Login is already taken (DB Constraint) ')
+
         db.refresh(new_user)
-
         return {"message": "registration_successful"}
-
+    except HTTPException:
+        raise
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+        raise HTTPException(status_code=500, detail="Internal Server Error!!!")
